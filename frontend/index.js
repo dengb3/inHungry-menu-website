5a799293186c88d988eb936ecdb7de229ef4c830
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-03-09 10:27:26 -0700

    deleted link of button & added eventListner

--- frontend/index.js
+++ frontend/index.js
@@ -2,3 +2,12 @@
 // title id1 = resArray[0].name
 // url id1 = resArray[0].img_url
 // description id1 = resArray[0].description
+
+
+document.getElementById("btn1").addEventListener("click", btn1Pressed);
+
+function btn1Pressed() {
+    fetch("http://localhost:3000/breakfast")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
